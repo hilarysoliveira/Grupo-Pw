96a745cc5d86877a3cc1a351b5d3cb1130bc8a01
Diego Viana <udiegoviana@gmail.com>
2023-06-18 06:17:29 -0400

    2 - 18/06/2023

--- js/scriptDiego.js
+++ js/scriptDiego.js
@@ -1,3 +1,5 @@
+document.getElementById("buttonEnviar").addEventListener("click", validar);
+
 function validar(event) {
     event.preventDefault(); // Impede o comportamento padrão do botão
 
@@ -65,4 +67,89 @@ function validar(event) {
     mensagemErro.textContent = "Enviado com sucesso!";
 }
 
-document.getElementById("buttonEnviar").addEventListener("click", validar);
+function checkbox() {
+
+    const simCheck = document.getElementById('simCheck');
+    const naoCheck = document.getElementById('naoCheck');
+    const inputNome = document.getElementById('inputNome');
+    const inputEmail = document.getElementById('inputEmail');
+    const inputCelular = document.getElementById('inputCelular');
+
+    if (simCheck.checked === true) {
+        if (naoCheck.checked === true) {
+            simCheck.checked = false;
+            naoCheck.checked = false;
+            mensagemErro.textContent = "Você não pode marcar ambas as opções.";
+            return;
+        }
+        else {
+            mensagemErro.textContent = "";
+        }
+    }
+    else {
+        mensagemErro.textContent = "";
+    }
+    if (naoCheck.checked === true) {
+        inputNome.disabled = true;
+        inputEmail.disabled = true;
+        inputCelular.disabled = true;
+    }
+    else {
+        inputNome.disabled = false;
+        inputEmail.disabled = false;
+        inputCelular.disabled = false;
+    }
+}
+
+document.getElementById("buttonEnviar2").addEventListener("click", validarFeedback);
+
+function validarFeedback() {
+
+    const simCheck = document.getElementById('simCheck');
+    const naoCheck = document.getElementById('naoCheck');
+    const inputNome = document.getElementById('inputNome');
+    const inputEmail = document.getElementById('inputEmail');
+    const inputCelular = document.getElementById('inputCelular');
+    const inputClassificacao = document.getElementById('inputClassficacao');
+    const inputClassificacao2 = document.getElementById('inputClassficacao2');
+    const inputClassificacao3 = document.getElementById('inputClassficacao3');
+    const mensagemErro = document.getElementById("mensagemErro");
+
+    if (simCheck.checked === false && naoCheck.checked == false) {
+        mensagemErro.textContent = "Você primeiro precisa escolher se quer se identificar ou não"
+        return
+    };
+
+    if (simCheck.checked === true) {
+        if (inputNome.value.length < 2 || inputNome.value.length > 50) {
+            mensagemErro.textContent = "Nome inválido. Insira um nome com 2 a 50 caracteres.";
+            return; // Interrompe a execução se houver erro
+        }
+        if (inputEmail.value.length < 5 || inputEmail.value.length > 50) {
+            mensagemErro.textContent = "E-mail inválido. Insira um e-mail com 5 a 50 caracteres.";
+            return; // interrompe a execução se houver erro
+        }
+    
+        // Validar formato do número de celular
+        const regexCelular = /^\d{10,20}$/;
+        if (!regexCelular.test(inputCelular.value)) {
+            mensagemErro.textContent = "Número de celular inválido. Insira um número de celular válido (apenas dígitos, no mínimo 10 e no máximo 20 caracteres).";
+            return; // interrompe a execução se houver erro
+        }
+    }
+    //verifica se todos os campos foram preenchidos
+    if (inputClassificacao.value.length === "" || inputClassificacao2.value.length === "" || inputClassificacao3.value.length === "") {
+        mensagemErro.textContent = "Para enviar o formulário é necessário responder às 3 perguntas de baixo.";
+        return;
+    }
+    mensagemErro.value = "Enviado com sucesso!";
+
+    inputNome.value = "";
+    inputEmail.value = "";
+    inputCelular.value = "";
+    inputClassificacao.value = "";
+    inputClassificacao2.value = "";
+    inputClassificacao3.value = "";
+    simCheck.checked = false;
+    naoCheck.checked = false;
+}
