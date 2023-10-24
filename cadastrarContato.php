d245c2e5aac063d396b0c0210f2d4c0005ed6086
Diego Viana <83664300+dieguviana@users.noreply.github.com>
2023-10-24 11:48:00 -0400

    Reorganizando o esquema de pastas

--- cadastrarContato.php
+++ cadastrarContato.php
@@ -4,21 +4,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../css/cadastrarContato.css">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="cadastrarContato.css">
     <title>Contatar HS</title>
 </head>
 <header>
-    <img src="../imagens/logo.png" alt="" class="logo">
+    <img src="imagens/logo.png" alt="" class="logo">
     <p>A fotografia tem o poder de eternizar a efemeridade</p>
     <nav class="topnav">
-        <a class="botaoM" href="../home.html">Home</a>
-        <a class="botaoM" href="../sobreNos.html">Sobre nós</a>
-        <a class="botaoM" href="../galeriaDeFotos.html">Galeria de fotos</a>
-        <a class="botaoM" href="../cadastrarContato.html">Entre em contato</a>
-        <a class="botaoM" href="../cadastrarEvento.html">Eventos</a>
-        <a class="botaoM" href="../cadastrarRevelacao.html">Revelações</a>
-        <a class="botaoM" href="../fazerLogin.html">Login</a>
+        <a class="botaoM" href="home.html">Home</a>
+        <a class="botaoM" href="sobreNos.html">Sobre nós</a>
+        <a class="botaoM" href="galeriaDeFotos.html">Galeria de fotos</a>
+        <a class="botaoM" href="cadastrarContato.html">Entre em contato</a>
+        <a class="botaoM" href="cadastrarEvento.html">Eventos</a>
+        <a class="botaoM" href="cadastrarRevelacao.html">Revelações</a>
+        <a class="botaoM" href="fazerLogin.html">Login</a>
     </nav>
 </header>
 
@@ -48,14 +48,14 @@
     }
     ?>
     <main>
-        <img src="../imagens/Foto1.jpg" alt="" class="images">
-        <img src="../imagens/Foto2.jpg" alt="" class="images">
-        <img src="../imagens/Foto3.jpg" alt="" class="images">
+        <img src="imagens/Foto1.jpg" alt="" class="images">
+        <img src="imagens/Foto2.jpg" alt="" class="images">
+        <img src="imagens/Foto3.jpg" alt="" class="images">
     </main>
 </body>
 
 <footer>
-    <a class="linkI" href="https://www.instagram.com/hilarysouzafotografias/"><img src="../imagens/logoInsta.png" alt="" class="logoInsta"></a>
+    <a class="linkI" href="https://www.instagram.com/hilarysouzafotografias/"><img src="imagens/logoInsta.png" alt="" class="logoInsta"></a>
     <a class="linkI" href="https://www.instagram.com/hilarysouzafotografias/" class="textoLink">@HilarySouzaFotografias</a>
     <p class="copy">&copy; 2023 Fotografias Inc.</p>
 </footer>
